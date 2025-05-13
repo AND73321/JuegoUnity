bb56fd63d1c6ce958455e77e1293db539a80914c
AndradeAlejandro <andrade73321@gmail.com>
2025-05-13 15:12:00 -0500

    Se actualizo el Debug  para probar la nueva animacion de giro de la carta

--- Assets/Scripts/DebugChangeCard.cs
+++ Assets/Scripts/DebugChangeCard.cs
@@ -2,31 +2,39 @@ using UnityEngine;
 
 public class DebugChangeCard : MonoBehaviour
 {
+    CardFlipper flipper;
     CardModel cardModel;
     int cardIndex = 0;
 
     public GameObject card;
 
-    private void Awake()
+    void Awake()
     {
         cardModel = card.GetComponent<CardModel>();
+        flipper = card.GetComponent<CardFlipper>();
     }
 
     void OnGUI()
     {
-        if (GUI.Button(new Rect(10,10,100,28), "Cambiar de Carta!"))
+        if (GUI.Button(new Rect(10,10,150,30), "Cambiar de Carta!"))
         {
 
             if (cardIndex >= cardModel.faces.Length)
             {
                 cardIndex = 0;
-                cardModel.ToggleFace(false);
+                flipper.FlipCard(cardModel.faces[cardModel.faces.Length -1], cardModel.cardBack, -1); 
             }
             else
             {
-                cardModel.cardIndex = cardIndex;
-                cardModel.ToggleFace(true);
-                cardIndex++;
+                if (cardIndex > 0)
+                {
+                    flipper.FlipCard(cardModel.faces[cardIndex - 1], cardModel.faces[cardIndex], cardIndex);
+                }
+                else
+                {
+                    flipper.FlipCard(cardModel.cardBack, cardModel.faces[cardIndex], cardIndex);
+                }
+                    cardIndex++;
             }
         }
     }
